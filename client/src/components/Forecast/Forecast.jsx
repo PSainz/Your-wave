47af22200bea449d2ac2c26dda9b8fb7d4f37885
Pepe <pepesainz2593@gmail.com>
2022-04-11 22:47:16 +0200

    Forecast api integration done

--- client/src/components/Forecast/Forecast.jsx
+++ client/src/components/Forecast/Forecast.jsx
@@ -1,174 +1,186 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CircularProgress } from "@mui/material";
+import hours from "../../utils/forecast.json";
+import "./styles.css";
 
-const Forecast = () => {
-  // const classes = useStyles();
+const Forecast = ({ spot }) => {
+  const forecast = hours.hours;
+  const loaded = true;
+
+  //   const [forecast, setForecast] = useState([]);
+  //   const [loaded, setLoaded] = useState(false);
+
+  //   useEffect(() => {
+  //     const getForecast = () => {
+  //       const params =
+  //         "airTemperature,precipitation,waterTemperature,waveDirection,waveHeight,wavePeriod,windDirection";
+
+  //       fetch(
+  //         `https://api.stormglass.io/v2/weather/point?lat=${spot.location.lat}&lng=${spot.location.lng}&params=${params}`,
+  //         {
+  //           headers: {
+  //             Authorization:
+  //               "807b335c-e8f5-11e9-80bf-0242ac130004-807b347e-e8f5-11e9-80bf-0242ac130004",
+  //           },
+  //         }
+  //       )
+  //         .then((response) => response.json())
+  //         .then((jsonData) => {
+  //           setForecast(jsonData.hours);
+  //           setLoaded(true);
+  //         });
+  //     };
+  //     getForecast();
+  //   }, []);
 
   return (
-    <div>
-      <div>
-        <h2>{spot.spot_name}</h2>
-      </div>
-      <div className="forecastInfo">
-        <div className="morning">
-          <div className="upRow">
-            <ul>
-              {!!forecast && (
+    <div className="forecastInfo">
+      <div className="morning">
+        {!!loaded ? (
+          <React.Fragment>
+            <div className="upRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">brightness_7</i> */}
                   {forecast[12].airTemperature.noaa.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-            <p>Morning 05 - 11h</p>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <p>Morning 05 - 11h</p>
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[12].waterTemperature.meto.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="midRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="midRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[4].waveHeight.meteo} -{" "}
                   {forecast[12].waveHeight.meteo} m
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="downRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="downRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[4].wavePeriod.meteo.toFixed()} -{" "}
                   {forecast[12].wavePeriod.meteo.toFixed()} s
                 </li>
-              )}
-            </ul>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <ul>
                 <li>
                   {forecast[4].windDirection.icon.toFixed()} -{" "}
                   {forecast[12].windDirection.icon.toFixed()} m/s
                 </li>
-              )}
-            </ul>
-          </div>
-        </div>
-        <div className="midDay">
-          <div className="upRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+          </React.Fragment>
+        ) : (
+          <CircularProgress />
+        )}
+      </div>
+      <div className="midDay">
+        {!!loaded ? (
+          <React.Fragment>
+            <div className="upRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">brightness_7</i> */}
                   {forecast[18].airTemperature.noaa.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-            <p>Mid day 11 - 17h</p>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <p>Mid day 11 - 17h</p>
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[18].waterTemperature.meto.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="midRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="midRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[12].waveHeight.meteo} -{" "}
                   {forecast[18].waveHeight.meteo} m
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="downRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="downRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[12].wavePeriod.meteo.toFixed()} -{" "}
                   {forecast[18].wavePeriod.meteo.toFixed()} s
                 </li>
-              )}
-            </ul>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <ul>
                 <li>
                   {forecast[12].windDirection.icon.toFixed()} -{" "}
                   {forecast[18].windDirection.icon.toFixed()} m/s
                 </li>
-              )}
-            </ul>
-          </div>
-        </div>
-        <div className="evening">
-          <div className="upRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+          </React.Fragment>
+        ) : (
+          <CircularProgress />
+        )}
+      </div>
+      <div className="evening">
+        {!!loaded ? (
+          <React.Fragment>
+            <div className="upRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">brightness_7</i> */}
                   {forecast[18].airTemperature.noaa.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-            <p>Evening 17 - 23h</p>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <p>Evening 17 - 23h</p>
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[24].waterTemperature.meto.toFixed()}
                   ºC
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="midRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="midRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[18].waveHeight.meteo} -{" "}
                   {forecast[24].waveHeight.meteo} m
                 </li>
-              )}
-            </ul>
-          </div>
-          <div className="downRow">
-            <ul>
-              {!!forecast && (
+              </ul>
+            </div>
+            <div className="downRow">
+              <ul>
                 <li>
                   {/* <i class="material-icons">opacity</i> */}
                   {forecast[18].wavePeriod.meteo.toFixed()} -{" "}
                   {forecast[24].wavePeriod.meteo.toFixed()} s
                 </li>
-              )}
-            </ul>
-            <ul>
-              {!!forecast && (
+              </ul>
+              <ul>
                 <li>
                   {forecast[18].windDirection.icon.toFixed()} -{" "}
                   {forecast[24].windDirection.icon.toFixed()} m/s
                 </li>
-              )}
-            </ul>
-          </div>
-        </div>
+              </ul>
+            </div>
+          </React.Fragment>
+        ) : (
+          <CircularProgress />
+        )}
       </div>
     </div>
   );
--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -5,8 +5,8 @@ import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 import Modal from "@mui/material/Modal";
 import Map from "./../Gmaps/Map.jsx";
-import hours from "../../utils/forecast.json";
-import "./styles.css";
+// import hours from "../../utils/forecast.json";
+import Forecast from "../Forecast/Forecast.jsx";
 
 const style = {
   //   position: "absolute",
@@ -29,29 +29,6 @@ export default function BasicModal({ spot }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const forecast = hours.hours;
-  console.log(forecast, "forecast");
-
-  // 807b335c-e8f5-11e9-80bf-0242ac130004-807b347e-e8f5-11e9-80bf-0242ac130004
-
-  // const getForecast = () => {
-  //   const params =
-  //     "airTemperature,precipitation,waterTemperature,waveDirection,waveHeight,wavePeriod,windDirection";
-
-  //   fetch(
-  //     `https://api.stormglass.io/v2/weather/point?lat=${spot.location.lat}&lng=${spot.location.lng}&params=${params}`,
-  //     {
-  //       headers: {
-  //         Authorization:
-  //           "807b335c-e8f5-11e9-80bf-0242ac130004-807b347e-e8f5-11e9-80bf-0242ac130004",
-  //       },
-  //     }
-  //   )
-  //     .then((response) => response.json())
-  //     .then((jsonData) => {
-  //       console.log(jsonData, "API FORECAST");
-  //     });
-  // };
 
   return (
     <div>
@@ -65,16 +42,6 @@ export default function BasicModal({ spot }) {
       >
         Show Forecast
       </Button>
-      {/* <Button
-        className={""}
-        variant="contained"
-        color="primary"
-        size="large"
-        type="fire"
-        onClick={getForecast}
-      >
-        Api call forecast
-      </Button> */}
       <Modal
         open={open}
         onClose={handleClose}
@@ -82,170 +49,7 @@ export default function BasicModal({ spot }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <div>
-            <h2>{spot.spot_name}</h2>
-          </div>
-          <div className="forecastInfo">
-            <div className="morning">
-              <div className="upRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">brightness_7</i> */}
-                      {forecast[12].airTemperature.noaa.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-                <p>Morning 05 - 11h</p>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[12].waterTemperature.meto.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="midRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[4].waveHeight.meteo} -{" "}
-                      {forecast[12].waveHeight.meteo} m
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="downRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[4].wavePeriod.meteo.toFixed()} -{" "}
-                      {forecast[12].wavePeriod.meteo.toFixed()} s
-                    </li>
-                  )}
-                </ul>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {forecast[4].windDirection.icon.toFixed()} -{" "}
-                      {forecast[12].windDirection.icon.toFixed()} m/s
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-            <div className="midDay">
-              <div className="upRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">brightness_7</i> */}
-                      {forecast[18].airTemperature.noaa.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-                <p>Mid day 11 - 17h</p>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[18].waterTemperature.meto.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="midRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[12].waveHeight.meteo} -{" "}
-                      {forecast[18].waveHeight.meteo} m
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="downRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[12].wavePeriod.meteo.toFixed()} -{" "}
-                      {forecast[18].wavePeriod.meteo.toFixed()} s
-                    </li>
-                  )}
-                </ul>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {forecast[12].windDirection.icon.toFixed()} -{" "}
-                      {forecast[18].windDirection.icon.toFixed()} m/s
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-            <div className="evening">
-              <div className="upRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">brightness_7</i> */}
-                      {forecast[18].airTemperature.noaa.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-                <p>Evening 17 - 23h</p>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[24].waterTemperature.meto.toFixed()}
-                      ºC
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="midRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[18].waveHeight.meteo} -{" "}
-                      {forecast[24].waveHeight.meteo} m
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="downRow">
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {/* <i class="material-icons">opacity</i> */}
-                      {forecast[18].wavePeriod.meteo.toFixed()} -{" "}
-                      {forecast[24].wavePeriod.meteo.toFixed()} s
-                    </li>
-                  )}
-                </ul>
-                <ul>
-                  {!!forecast && (
-                    <li>
-                      {forecast[18].windDirection.icon.toFixed()} -{" "}
-                      {forecast[24].windDirection.icon.toFixed()} m/s
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-          </div>
+          <Forecast spot={spot} />
           <Map location={spot.location} zoomLevel={15} />
         </Box>
       </Modal>
